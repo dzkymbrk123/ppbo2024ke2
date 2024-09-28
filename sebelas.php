7a72f9fe588ad9e2d58d54eccdebfee3f5539d7e
dzkymbrk123 <dzakyjr1@gmail.com>
2024-09-28 18:47:54 +0700

    "Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance"

--- sebelas.php
+++ sebelas.php
@@ -1,15 +1,25 @@
 <?php
-class Dosen {
+class Pegawai {
+    protected $no_hp;
+
+    public function __construct($no_hp) {
+        $this->no_hp = $no_hp;
+    }
+
+    protected function getNoHp() {
+        return $this->no_hp;
+    }
+}
+
+class Dosen extends Pegawai {
     public $nama;
     public $nip;
-    public $no_hp;
     public $alamat;
     public $nidn;
 
-    public function __construct($nama, $nip, $no_hp, $alamat, $nidn) {
+    public function __construct($nama, $nip, $alamat, $nidn) {
         $this->nama = $nama;
         $this->nip = $nip;
-        $this->no_hp = $no_hp;
         $this->alamat = $alamat;
         $this->nidn = $nidn;
     }
@@ -17,13 +27,13 @@ class Dosen {
     public function tampilkanInfo() {
         echo "Nama: " . $this->nama . "<br>";
         echo "NIP: " . $this->nip . "<br>";
-        echo "No HP: " . $this->no_hp . "<br>";
         echo "Alamat: " . $this->alamat . "<br>";
         echo "NIDN: " . $this->nidn . "<br>";
+        echo "No HP: " . $this->getNoHp() . "<br>"; 
     }
 }
 
-$dian = new Dosen("Dian Prawira", "198411132015041001", "62111111", "Jln Purnama", "0013118405");
+$dian = new Dosen("Dian Prawira", "198411132015041001", "Jln Purnama", "0013118405");
 
 $dian->tampilkanInfo();
 ?>
